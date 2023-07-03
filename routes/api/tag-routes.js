2f20121791ccf014b63337503bcc542d73e8f9a9
Chandler Chilvers <cwchilvers@gmail.com>
2023-07-03 14:04:09 -0700

    Find all tages & get tag by ID requests

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,45 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  tag = Tag.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+        as: 'product_tags'
+      }
+    ]
+  })
+  // Send response
+  .then((tag) => {
+    res.json(tag);  // Send tag data to client
+  })
+  .catch((err) => {
+    res.json(err);  // Send error to client
+  });
+  
+
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  tag = Tag.findByPk(req.params.id, {   // find by primary key (id)
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+        as: 'product_tags'
+      }
+    ]
+  })
+  // Send response
+  .then((tag) => {
+      res.json(tag);  // Send tag data to client
+  })
+  .catch((err) => {
+      res.json(err);  // Send error to client
+  });
 });
 
 router.post('/', (req, res) => {
